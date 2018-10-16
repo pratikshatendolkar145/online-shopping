f3bd8d4ccd4a179096ab33a85b82519cd59c0adf
pratiksha tendolkar <pratikshatendolkar145@gmail.com>
2018-10-16 12:59:06 +0530

    frontend and backend created successfully.

---
+++ shoppingbackend/src/main/java/com/pra/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.pra.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
